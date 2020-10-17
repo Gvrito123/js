32b2fdcd59d95ab5661a79cc725218aaa8b0f266
Gvrito123 <gvritagio@gmail.com>
2020-10-17 15:27:44 +0400

    bugfix + added functionality in js

--- script.js
+++ script.js
@@ -1,32 +1,88 @@
-let res = document.querySelector("#res")
-let clickables = document.querySelectorAll('.clickable');
-var num1 = 0;
-var num2 = 0;
-var num3 = 0;
-function sum(p1,p2) {
-    return p1+p2;
+var resultVisual = document.querySelector("#res")
+var clickables = document.querySelectorAll('.clickable');
+var num1 = '';
+var num2 = '';
+var resultNumber = 0;
+var action = '';
+
+
+
+function sum(p1,p2){
+    return parseInt(p1)+ parseInt(p2);
+}
+function multiply(p1,p2){
+    return parseFloat(p1)*parseFloat(p2);
+}
+function divide(p1,p2) {
+    return parseFloat(p1)/parseFloat(p2);
+}
+function substract(p1,p2) {
+    return parseInt(p1)-parseInt(p2);
 }
 clickables.forEach((elem) => {
-    elem.addEventListener("click", function clickButton(button) {
-        if (this.dataset.val == "ac"){ 
-            res.textContent = 0;
+    elem.addEventListener("click", function clickButton() {
+        if (num2 === ''){
+            document.getElementById('op-eq').disabled = true;
         }
-         else {
-             res.textContent = this.dataset.val;
-         }
-        if (this.dataset.val>=0){
-            num2=num1;
-            num1=this.dataset.val;
+        if (this.dataset.val == "ac") {
+            resultVisual.textContent ='RES';
+            num1 = '';
+            num2 = '';
+            resultNumber = 0;
+            document.getElementById('op-eq').disabled = false;
         }
-        console.log(num1);
-        console.log(num2);
-        num1=+num1;
-        num2=+num2;
-        if (this.dataset.val == "="){ 
-            num3 = num1 + num2;
-            console.log(num1+num2);
-            res.textContent = num3;
+        else if (this.dataset.val !== "+" && this.dataset.val !== '=' && this.dataset.val !== '*' && this.dataset.val !== '/' && this.dataset.val !== '-') {
+            if (action === '')
+            {
+                num1 += this.dataset.val;
+                resultVisual.textContent = num1;
+            }
+            else
+            {   
+                num2 += this.dataset.val;
+                resultVisual.textContent = num2;
+                document.getElementById('op-eq').disabled = false;
+            }
         }
-        
+        else if (this.dataset.val == "+"){
+            resultVisual.textContent = "+";
+            action = "+";
+        }
+        else if (this.dataset.val == "*") {
+            resultVisual.textContent= '*';
+            action = '*';
+        }
+        else if (this.dataset.val == '/') {
+            resultVisual.textContent = '/';
+            action = '/';
+        }
+        else if (this.dataset.val == '-'){
+            resultVisual.textcontent = '-';
+            action = '-';
+        }
+        if (this.dataset.val == "=" && action == "+"){
+            resultVisual.textContent = sum(num1,num2);
+            num1 = parseInt(resultVisual.textContent);
+            num2 = '';
+            action = '';
+        }
+        else if(this.dataset.val == '=' && action == '*'){
+            resultVisual.textContent = multiply(num1,num2);
+            num1 = parsefloat(resultVisual.textContent);
+            num2 = '';
+            action = '';
+        }
+        else if(this.dataset.val == '=' && action == '/'){
+            resultVisual.textContent = divide(num1,num2);
+            num1 = parseFloat(resultVisual.textContent);
+            num2 = '';
+            action = '';
+        }
+        else if(this.dataset.val == '=' && action == '-'){
+            resultVisual.textContent = substract(num1,num2);
+            num1 = parseInt(resultVisual.textContent);
+            num2 = '';
+            action = '';
+        }
     });
 })
